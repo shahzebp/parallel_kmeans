24e8d95ed93245007f4d0a5eb80702e46eb2f194
shahzebp <shahpatel@cs.stonybrook.edu>
2016-05-15 03:26:39 -0500

    cleanup

--- opencl/kmeans.h
+++ opencl/kmeans.h
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 #define FLT_MAX 3.40282347e+38
-=======
-#ifndef _KMEANS
-#define _KMEANS
->>>>>>> db787061f229d62667314980345e922711c1c665
 
 int setup(int argc, char** argv);
 int allocate(int npoints, int nfeatures, int nclusters, float **feature);
