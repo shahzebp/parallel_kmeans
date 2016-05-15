06132d4ce009684cf699d3861cf5555c87053d70
Akshit Poddar <tg833245@login1.stampede.tacc.utexas.edu>
2016-05-15 16:06:15 -0500

    Commiting Change

--- opencl/kmeans.h
+++ opencl/kmeans.h
@@ -3,7 +3,6 @@
 
 int setup(int argc, char** argv);
 int allocate(int npoints, int nfeatures, int nclusters, float **feature);
-void deallocateMemory();
 int	kmeansOCL(float **feature, int nfeatures, int npoints, int nclusters, int *membership, float **clusters, int *new_centers_len, float  **new_centers);
 
 #endif
