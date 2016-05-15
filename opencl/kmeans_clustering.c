d61da1667ed6b359aabdaa9826cd9206ea7107b8
Akshit Poddar <tg833245@login1.stampede.tacc.utexas.edu>
2016-05-15 01:40:46 -0500

    Commiting Change

--- opencl/kmeans_clustering.c
+++ opencl/kmeans_clustering.c
@@ -7,40 +7,28 @@
 
 #define RANDOM_MAX 2147483647
 
-/*----< kmeans_clustering() >---------------------------------------------*/
-float** kmeans_clustering(float **feature,    /* in: [npoints][nfeatures] */
-                          int     nfeatures,
-                          int     npoints,
-                          int     nclusters,
-                          float   threshold,
-                          int    *membership) /* out: [npoints] */
+float** kmeans_clustering(float **feature, int nfeatures, int npoints,
+							int nclusters, float threshold, int *membership)
 {    
-    int      i, j, n = 0;				/* counters */
+    int      i, j, n = 0;
 	int		 loop=0, temp;
-    int     *new_centers_len;	/* [nclusters]: no. of points in each cluster */
-    float    delta;				/* if the point moved */
-    float  **clusters;			/* out: [nclusters][nfeatures] */
-    float  **new_centers;		/* [nclusters][nfeatures] */
+    int     *new_centers_len;
+    float    delta;
+    float  **clusters;
+    float  **new_centers;
 
-	int     *initial;			/* used to hold the index of points not yet selected
-								   prevents the "birthday problem" of dual selection (?)
-								   considered holding initial cluster indices, but changed due to
-								   possible, though unlikely, infinite loops */
+	int     *initial;
 	int      initial_points;
 	int		 c = 0;
 
-	/* nclusters should never be > npoints
-	   that would guarantee a cluster without points */
 	if (nclusters > npoints)
 		nclusters = npoints;
 
-    /* allocate space for and initialize returning variable clusters[] */
     clusters    = (float**) malloc(nclusters *             sizeof(float*));
     clusters[0] = (float*)  malloc(nclusters * nfeatures * sizeof(float));
     for (i=1; i<nclusters; i++)
         clusters[i] = clusters[i-1] + nfeatures;
 
-	/* initialize the random clusters */
 	initial = (int *) malloc (npoints * sizeof(int));
 	for (i = 0; i < npoints; i++)
 	{
@@ -48,15 +36,11 @@ float** kmeans_clustering(float **feature,    /* in: [npoints][nfeatures] */
 	}
 	initial_points = npoints;
 
-    /* randomly pick cluster centers */
     for (i=0; i<nclusters && initial_points >= 0; i++) {
-		//n = (int)rand() % initial_points;		
 		
         for (j=0; j<nfeatures; j++)
-            clusters[i][j] = feature[initial[n]][j];	// remapped
+            clusters[i][j] = feature[initial[n]][j];
 
-		/* swap the selected index to the end (not really necessary,
-		   could just move the end up) */
 		temp = initial[n];
 		initial[n] = initial[initial_points-1];
 		initial[initial_points-1] = temp;
@@ -64,11 +48,9 @@ float** kmeans_clustering(float **feature,    /* in: [npoints][nfeatures] */
 		n++;
     }
 
-	/* initialize the membership to -1 for all */
     for (i=0; i < npoints; i++)
 	  membership[i] = -1;
 
-    /* allocate space for and initialize new_centers_len and new_centers */
     new_centers_len = (int*) calloc(nclusters, sizeof(int));
 
     new_centers    = (float**) malloc(nclusters *            sizeof(float*));
@@ -76,25 +58,21 @@ float** kmeans_clustering(float **feature,    /* in: [npoints][nfeatures] */
     for (i=1; i<nclusters; i++)
         new_centers[i] = new_centers[i-1] + nfeatures;
 
-	/* iterate until convergence */
 	do {
         delta = 0.0;
-		// CUDA
 		delta = (float) kmeansOCL(feature, nfeatures, npoints, nclusters,
 								membership, clusters, new_centers_len, new_centers);
 
-		/* replace old cluster centers with new_centers */
-		/* CPU side of reduction */
 		for (i=0; i<nclusters; i++) {
 			for (j=0; j<nfeatures; j++) {
 				if (new_centers_len[i] > 0)
-					clusters[i][j] = new_centers[i][j] / new_centers_len[i];	/* take average i.e. sum/n */
-				new_centers[i][j] = 0.0;	/* set back to 0 */
+					clusters[i][j] = new_centers[i][j] / new_centers_len[i];
+				new_centers[i][j] = 0.0;
 			}
-			new_centers_len[i] = 0;			/* set back to 0 */
+			new_centers_len[i] = 0;
 		}	 
 		c++;
-    } while ((delta > threshold) && (loop++ < 500));	/* makes sure loop terminates */
+    } while ((delta > threshold) && (loop++ < 500));
 	printf("iterated %d times\n", c);
     free(new_centers[0]);
     free(new_centers);
