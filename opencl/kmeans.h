ba520dba8655ca6248f4945a100379c565b7e453
shahzeb <shahzeb@localhost.localdomain>
2016-05-15 21:03:27 -0400

    changes

--- opencl/kmeans.h
+++ opencl/kmeans.h
@@ -3,6 +3,6 @@
 
 int setup(int argc, char** argv);
 int allocate(int npoints, int nfeatures, int nclusters, float **feature);
-int	kmeansOCL(float **feature, int nfeatures, int npoints, int nclusters, int *membership, float **clusters, int *new_centers_len, float  **new_centers);
+int	k_means_CL(float **feature, int nfeatures, int npoints, int nclusters, int *membership, float **clusters, int *new_centers_len, float  **new_centers);
 
 #endif
